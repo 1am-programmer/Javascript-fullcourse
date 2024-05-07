1eb3dd22c8d8ffeeb43240a0b58a16498f665c60
Your Name <dannydotdev@gmail.com>
2024-05-07 17:45:26 +0100

    functions

---
+++ 08Function.js
@@ -0,0 +1,10 @@
+//Stand in line
+/*
+A cue is an abstract data structure, where items are kept in order..
+New items are added to the back of the cue and old items are taken off from the cue,
+|| eg. the shift and push functions
+ */
+
+function nextInLine(arr, item) {
+  return item;
+}
