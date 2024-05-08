d1bd84ae30b6835867fc151498c15a4bbb0de152
Your Name <dannydotdev@gmail.com>
2024-05-08 12:08:25 +0100

    Switch case

--- 14Switchcase.js
+++ 14Switchcase.js
@@ -3,7 +3,6 @@
 A switch statement tests a value and can have many case statements which define various possible values
 */
 function switchCase(day) {
-  var day = 5;
   switch (day) {
     case 1:
       console.log("Monday");
@@ -30,7 +29,7 @@ function switchCase(day) {
       console.log("Invalid day");
   }
 }
-console.log(switchCase(7));
+switchCase(7);
 
 /*Means if the value passed in day is 1, print Monday, and if the value passed is 2, print Tuesday
 If the value passed is 3, print Wednesday, and so on.
@@ -39,3 +38,26 @@ THIS METHOD USES THE STRICT OPERATOR === , so 1 and "1" are not the same
 
 //Break means we are at the end of the case statement, if you do not add a break statement, it will just run all the codes automatically
 //The default is like the else in an if-else statement, if all cases are false, then run the default
+
+/*
+Switch case with multiple identical option statements
+*/
+
+function identicalCase(day) {
+  switch (day) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+      console.log("Weekday");
+      break;
+    case 6:
+    case 7:
+      console.log("Weekend");
+      break;
+    default:
+      console.log("Invalid day");
+  }
+}
+identicalCase(5);
