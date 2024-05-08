9e4326c759f45c40b85a1fd7fbd97297102077e0
Your Name <dannydotdev@gmail.com>
2024-05-08 20:48:31 +0200

    Nesting and loops

--- 20Loop.js
+++ 20Loop.js
@@ -30,7 +30,7 @@ And it keeps looping until the last integer less than 10 is gotten
 
 var arr2 = [1, 2, 3, 4, 5, 6, 7, 8];
 for (let index = 1; index < arr2.length; index++) {
-  const element = arr[index];
+  const element = arr2[index];
   console.log(element);
 }
 console.log(arr2);
@@ -43,8 +43,21 @@ in the for loop,
 
 - expression => index++ for every iteration of the loop, increment index by 1
 
-- const element = array[index]  which means element is reinitializing the items of the array to the values printed in the index ++ i.e 1-7
+- const element = arr2[index]  which means element is reinitializing the items of the array to the values printed in the index ++ i.e 1-7
 - and its been printed out in the console.log(element);
 
 - console.log(arr2)=> finally print out the arr 
+*/
+
+/*
+
+ var arr2= [1, 2, 3, 4, 5, 6, 7, 8];: This line initializes an array arr with values from 1 to 8.
+The for loop iterates over each element in the arr array.
+let index = 0;: Initialize a variable index with value 0.
+index < arr.length;: The loop continues as long as index is less than the length of the array arr.
+index++: Increment index by 1 in each iteration.
+const element = arr2[index];: Inside the loop, it retrieves the element at the current index in the array arr and assigns it to a constant variable element.
+console.log(element);: It prints each element of arr to the console.
+After the loop finishes, console.log(arr2); prints the entire array arr to the console.
+So, when you run this code, you'll see each element of the arr array printed to the console individually, followed by the entire array printed again
 */
