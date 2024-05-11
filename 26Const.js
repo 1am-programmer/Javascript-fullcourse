3a5e6243d9bca6e9ddb23abe4c6706fc5786e43d
Your Name <dannydotdev@gmail.com>
2024-05-11 20:25:41 +0200

    preventing objects mutation

--- 26Const.js
+++ 26Const.js
@@ -26,4 +26,18 @@ console.log(LANG);
 //Prevent Object Mutation
 /*
 The earlier example, makes it clear that even an array declared with const can be mutated
+
+A way of preventing object preventing object mutation in Javascript is through object.freeze 
 */
+const MATH_PI = {
+  pi: 3.14,
+  e: 2.71,
+};
+Object.freeze(MATH_PI); //By commenting this, the values in the objects can be changed
+
+try {
+  MATH_PI.e = 3.89; //This will change the value of e from 2.71 to 3.89, but with adding the object.freeze after the objects, no value can be changes
+} catch (ex) {
+  console.log(ex);
+}
+console.log(MATH_PI);
