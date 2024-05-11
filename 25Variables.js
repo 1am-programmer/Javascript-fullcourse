02da9db68e82d291469e3a29978201a31c0c0ba6
Your Name <dannydotdev@gmail.com>
2024-05-11 18:33:58 +0200

    block and local scope

--- 25Variables.js
+++ 25Variables.js
@@ -15,7 +15,7 @@ console.log(names); //Output Typescript
 
 //While let
 let language = "Javascript";
-// let language = "Typescript";
+// let language = "Typescript";  Uncomment this
 console.log(language); //Output Error
 
 //But if we reinitialize without using the keyword let, it does not throw an error
@@ -23,29 +23,42 @@ language = "C++";
 console.log(language);
 //Output C++ Because variables can be reinitialised but not declared twice using same let keyword
 
-/*"Use strict" :- This enables strict mode which catches common coding mistakes and unsafe actions, it can 
-be used in a top of the file or in a function
-e.g if you write a variable and don't declare it with var, let and const
-*/
+/*"Use strict" :- This enables strict mode which catches common coding mistakes and unsafe actions in Javascript, it can 
+be used at the top of the file or in a function
+e.g if you write a variable and don't declare it with var, let and const, it prompts an error */
 
 //SCOPES OF KEYWORDS
 /*
 If a variable is declared with VAR, it is declared globally and can be used anywhere, or locally when it is declared inside in a function
-If a variable is declared with LET, it is declared locally and can only be used inside the block in which it is declared
-*/
+If a variable is declared with LET, it is declared locally and can only be used inside the block in which it is declared*/
 
 //FOR VAR
 function checkScope() {
   "use strict";
-  var i = "function scope";
+  // var i = "function scope";
   if (true) {
     var i = "block scope";
-    console.log("Block Scope i is ", i); //Output => Block scope is stll block scope
+    console.log("Block Scope i is ", i); //Output => Block scope is block scope
   }
-  console.log("Function scope", i);
+  console.log("Function scope is ", i); //Output => Function scope is block scope, because we can still access a var keyword outside the block/local scope
   return i;
 }
-checkScope(); //Output => Function scope is stll block scope
+checkScope();
 
 //FOR LET
-function checkBlock() {}
+function checkBlock() {
+  "use strict";
+  // let j = "function scope";
+  if (true) {
+    let j = "block scope";
+    console.log("Block Scope j is ", j); //Output => Block scope is block scope
+  }
+  console.log("Function scope j is ", j); //Output j is not defined, because it cannot be referrenced outisde the if statement
+  return j; //Output j is not defined, because it cannot be referrenced outisde the if statement
+}
+checkBlock();
+
+//CONST
+/*
+Const has all the attributes of let, but it cannot be reassigned
+*/
