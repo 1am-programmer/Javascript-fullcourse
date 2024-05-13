8148553d28bc917c5826cacbd6ec34e4af501bff
Your Name <dannydotdev@gmail.com>
2024-05-13 11:40:36 +0200

    destructuring with objects and nested objects

--- 29Destructuring.js
+++ 29Destructuring.js
@@ -16,3 +16,38 @@ function results() {
   console.log(vowels);
 }
 results(); //Output { a: 1, e: 5, i: 9, o: 15, u: 21 };
+//Using the destructuring method to get the values from objects and assign them to another variable
+
+const TEST_SCORES = {
+  highest: 79,
+  lowest: 10,
+};
+
+function getHighest(result) {
+  "use strict";
+  const { highest: theHighest } = result;
+  //Gets the highest field from the result's object and then store it in theHighest variable
+  return theHighest;
+}
+console.log(getHighest(TEST_SCORES)); //Output 79
+//The function takes the TEST_SCORES as the argument passed for the parameter results
+
+//DESTRUCTING ASSIGNMENTS WITH NESTED OBJECTS
+const nestedObj = {
+  maths: { best: 98, worst: 46 },
+  english: { best: 95, worst: 32 },
+};
+
+function bestInMaths(subj) {
+  "use strict";
+  const {
+    maths: { best: theBest },
+  } = subj;
+  return theBest;
+}
+console.log(bestInMaths(nestedObj)); //Output 98
+
+/*To get the best in maths, with the destructuring method, 
+  => first you take maths out , then assign it to another curly brace
+  => and then take the best out of the maths object and assign it to theBest variable
+  */
